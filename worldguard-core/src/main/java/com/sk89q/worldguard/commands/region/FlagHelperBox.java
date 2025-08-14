179d9224274c40db4320e2c4dc8c7eaa43f32346
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-14 23:20:00 +0300

    Update FlagHelperBox.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/region/FlagHelperBox.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/region/FlagHelperBox.java
@@ -84,7 +84,7 @@ class FlagHelperBox extends PaginationBox {
     private boolean monoSpace;
 
     FlagHelperBox(World world, ProtectedRegion region, RegionPermissionModel perms) {
-        super("Flags for " + region.getId(), "/rg flags -w \"" + world.getName() + "\" -p %page% " + region.getId());
+        super("Прапори для " + region.getId(), "/rg flags -w \"" + world.getName() + "\" -p %page% " + region.getId());
         this.world = world;
         this.region = region;
         this.perms = perms;
@@ -93,7 +93,7 @@ class FlagHelperBox extends PaginationBox {
     @Override
     public Component getComponent(int number) {
         if (number == Flags.INBUILT_FLAGS.size()) {
-            return centerAndBorder(TextComponent.of("Third-Party Flags", TextColor.AQUA));
+            return centerAndBorder(TextComponent.of("Прапори третіх сторін", TextColor.AQUA));
         } else if (number > Flags.INBUILT_FLAGS.size()) {
             number -= 1;
         }
@@ -121,13 +121,13 @@ class FlagHelperBox extends PaginationBox {
         if (flag.usesMembershipAsDefault()) {
             builder.append(TextComponent.empty().append(TextComponent.of("*", TextColor.AQUA))
                     .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT,
-                            TextComponent.of("This is a special flag which defaults to allow for members, and deny for non-members"))));
+                            TextComponent.of("Це спеціальний прапор, який за замовчуванням дає доступ для учасників та забороняє доступ для не учасників."))));
             length += monoSpace ? 1 : FlagFontInfo.getPxLength('*');
         }
         if (flag == Flags.PASSTHROUGH) {
             builder.append(TextComponent.empty().append(TextComponent.of("*", TextColor.AQUA))
                     .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT,
-                            TextComponent.of("This is a special flag which overrides build checks. (Not movement related!)"))));
+                            TextComponent.of("Це спеціальний прапор, який замінює перевірки будівлі. (Не пов'язаний з рухом!)"))));
             length += monoSpace ? 1 : FlagFontInfo.getPxLength('*');
         }
         int leftover = (monoSpace ? PAD_PX_SIZE / 3 : PAD_PX_SIZE) - length;
@@ -199,15 +199,15 @@ class FlagHelperBox extends PaginationBox {
             List<Component> hoverTexts = new ArrayList<>();
             if (maySet) {
                 if (isExplicitSet) {
-                    hoverTexts.add(TextComponent.of("Click to unset", TextColor.GOLD));
+                    hoverTexts.add(TextComponent.of("Натисніть, щоб скинути", TextColor.GOLD));
                 } else if (DANGER_ZONE.contains(flag) && !(ProtectedRegion.GLOBAL_REGION.equals(region.getId()) && flag == Flags.PASSTHROUGH)) {
-                    hoverTexts.add(TextComponent.of("Setting this flag may have unintended consequences.", TextColor.RED)
+                    hoverTexts.add(TextComponent.of("Встановлення цього прапорця може мати непередбачувані наслідки.", TextColor.RED)
                             .append(TextComponent.newline())
-                            .append(TextComponent.of("Please read the documentation and set this flag manually if you really intend to.")
+                            .append(TextComponent.of("Будь ласка, прочитайте документацію та встановіть цей прапор вручну, якщо ви дійсно маєте намір це зробити.")
                             .append(TextComponent.newline())
-                            .append(TextComponent.of("(Hint: You do not need to set this to protect the region!)"))));
+                            .append(TextComponent.of("(Підказка: Вам не потрібно встановлювати це, щоб захистити регіон!)"))));
                 } else {
-                    hoverTexts.add(TextComponent.of("Click to set", TextColor.GOLD));
+                    hoverTexts.add(TextComponent.of("Натисніть, щоб встановити", TextColor.GOLD));
                 }
             }
             Component valType = getToolTipHint(defVal, choice, inherited);
@@ -238,7 +238,7 @@ class FlagHelperBox extends PaginationBox {
         if (suggestChoice != null && perms.maySetFlag(region, flag)) {
             builder.append(TextComponent.of(suggestChoice, TextColor.DARK_GRAY)
                     .hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT,
-                            TextComponent.of("Click to set custom value", TextColor.GOLD)))
+                            TextComponent.of("Натисніть, щоб встановити власне значення", TextColor.GOLD)))
                     .clickEvent(ClickEvent.of(ClickEvent.Action.SUGGEST_COMMAND, makeCommand(flag, ""))));
         }
     }
@@ -266,9 +266,9 @@ class FlagHelperBox extends PaginationBox {
         List<Component> hoverTexts = new ArrayList<>();
         if (maySet) {
             if (isExplicitSet) {
-                hoverTexts.add(TextComponent.of("Click to change", TextColor.GOLD));
+                hoverTexts.add(TextComponent.of("Натисніть, щоб змінити", TextColor.GOLD));
             } else {
-                hoverTexts.add(TextComponent.of("Click to set", TextColor.GOLD));
+                hoverTexts.add(TextComponent.of("Натисніть, щоб встановити", TextColor.GOLD));
             }
         }
         Component valType = getToolTipHint(defVal, currVal, inherited);
@@ -319,19 +319,19 @@ class FlagHelperBox extends PaginationBox {
         Component valType;
         if (inherited) {
             if (currVal == defVal) {
-                valType = TextComponent.of("Inherited & ")
-                        .append(TextComponent.of("default")
+                valType = TextComponent.of("Успадковане & ")
+                        .append(TextComponent.of("за замовчуванням")
                                 .decoration(TextDecoration.UNDERLINED, true))
-                        .append(TextComponent.of(" value"));
+                        .append(TextComponent.of(" значення"));
             } else {
-                valType = TextComponent.of("Inherited value");
+                valType = TextComponent.of("Успадковане значення");
             }
         } else {
             if (currVal == defVal) {
                 valType = TextComponent.empty()
-                        .append(TextComponent.of("Default")
+                        .append(TextComponent.of("За замовчуванням")
                                 .decoration(TextDecoration.UNDERLINED, true))
-                        .append(TextComponent.of(" value"));
+                        .append(TextComponent.of(" значення"));
             } else {
                 valType = null;
             }
@@ -365,7 +365,7 @@ class FlagHelperBox extends PaginationBox {
                 : currVal.stream().map(String::valueOf).collect(Collectors.joining(","));
         TextComponent hoverComp = TextComponent.of("");
         if (currVal != null) {
-            hoverComp = hoverComp.append(TextComponent.of("Current values:"))
+            hoverComp = hoverComp.append(TextComponent.of("Поточні значення:"))
                     .append(TextComponent.newline()).append(TextComponent.of(stringValue));
         }
         appendValueText(builder, flag, display, hoverComp);
@@ -390,13 +390,13 @@ class FlagHelperBox extends PaginationBox {
         if (currVal == null) {
             final Location defVal = flag.getDefault();
             if (defVal == null) {
-                appendValueText(builder, flag, "unset location", null);
+                appendValueText(builder, flag, "невизначена локація", null);
             } else {
-                appendValueText(builder, flag, defVal.toString(), TextComponent.of("Default value:")
+                appendValueText(builder, flag, defVal.toString(), TextComponent.of("За замовчуванням значення:")
                         .append(TextComponent.newline()).append(TextComponent.of(defVal.toString())));
             }
         } else {
-            appendValueText(builder, flag, currVal.toString(), TextComponent.of("Current value:")
+            appendValueText(builder, flag, currVal.toString(), TextComponent.of("Поточне значення:")
                     .append(TextComponent.newline()).append(TextComponent.of(currVal.toString())));
         }
     }
@@ -419,7 +419,7 @@ class FlagHelperBox extends PaginationBox {
             choices.addAll(Arrays.asList(suggested));
         }
         //noinspection unchecked
-        appendValueChoices(builder, flag, (Iterator<V>) choices.iterator(), choices.isEmpty() ? "unset number" : "[custom]");
+        appendValueChoices(builder, flag, (Iterator<V>) choices.iterator(), choices.isEmpty() ? "невизначена цифра" : "[власний]");
     }
 
     private void appendStringFlagValue(TextComponent.Builder builder, StringFlag flag) {
@@ -430,7 +430,7 @@ class FlagHelperBox extends PaginationBox {
         if (currVal == null) {
             final String defVal = flag.getDefault();
             if (defVal == null) {
-                appendValueText(builder, flag, "unset string", null);
+                appendValueText(builder, flag, "невизначений рядок", null);
             } else {
                 final TextComponent defComp = LegacyComponentSerializer.INSTANCE.deserialize(defVal);
                 String display = reduceToText(defComp);
@@ -438,7 +438,7 @@ class FlagHelperBox extends PaginationBox {
                 if (display.length() > 23) {
                     display = display.substring(0, 20) + "...";
                 }
-                appendValueText(builder, flag, display, TextComponent.of("Default value:")
+                appendValueText(builder, flag, display, TextComponent.of("За замовчуванням значення:")
                         .append(TextComponent.newline()).append(defComp));
             }
         } else {
@@ -448,7 +448,7 @@ class FlagHelperBox extends PaginationBox {
             if (display.length() > 23) {
                 display = display.substring(0, 20) + "...";
             }
-            appendValueText(builder, flag, display, TextComponent.of("Current value:")
+            appendValueText(builder, flag, display, TextComponent.of("Поточне значення:")
                     .append(TextComponent.newline()).append(currComp));
         }
     }
