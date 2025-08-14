dd10fc3aacc35f9a77937b851c13dac88348d3b5
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-14 22:37:34 +0300

    Update PaperProfileService.java

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/util/profile/resolver/PaperProfileService.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/util/profile/resolver/PaperProfileService.java
@@ -36,7 +36,7 @@ public final class PaperProfileService extends SingleRequestService {
 
     private PaperProfileService() {
         if (!PaperLib.isPaper()) {
-            throw new IllegalStateException("Attempt to access PaperProfileService on non-Paper server.");
+            throw new IllegalStateException("Спроба доступу PaperProfileService на не-Paper сервері.");
         }
     }
 
