c888d8d525f3d8712ad351f3392825bbe99522af
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-14 20:27:41 +0300

    Update Cause.java

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/cause/Cause.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/cause/Cause.java
@@ -253,7 +253,7 @@ public final class Cause {
      */
     public static void trackParentCause(Metadatable target, Object parent) {
         if (target instanceof Block) {
-            throw new IllegalArgumentException("Can't track causes on Blocks because Cause doesn't check block metadata");
+            throw new IllegalArgumentException("Неможливо відстежити причини у блоках, оскільки Cause не перевіряє метадані блоків.");
         }
 
         WGMetadata.put(target, CAUSE_KEY, parent);
