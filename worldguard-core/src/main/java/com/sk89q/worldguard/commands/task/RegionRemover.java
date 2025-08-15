be9fa468e3ac1cff7a5968a41b1721fcb8778cdc
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 11:32:10 +0300

    Update RegionRemover.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/task/RegionRemover.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/task/RegionRemover.java
@@ -80,8 +80,8 @@ public class RegionRemover implements Callable<Set<ProtectedRegion>> {
                 ProtectedRegion parent = test.getParent();
                 if (parent != null && parent.equals(region)) {
                     throw new CommandException(
-                            "The region '" + region.getId() + "' has child regions. Use -f to force removal of children " +
-                                    "or -u to unset the parent value of these children.");
+                            "Регіон '" + region.getId() + "' має спадкуючий регіони. Використовуйте -f для примусового вилучення спадкуючих " +
+                                    "або -u щоб скасувати батьківське значення у спадкуючих.");
                 }
             }
 
