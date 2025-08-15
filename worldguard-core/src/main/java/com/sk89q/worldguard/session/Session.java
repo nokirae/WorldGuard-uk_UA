129f8055d0920f87988a54daf18caebdbf0537a7
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 15:56:25 +0300

    Update Session.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/session/Session.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/session/Session.java
@@ -112,8 +112,8 @@ public class Session {
         disableBypass = cfg.disableDefaultBypass;
         if (cfg.announceBypassStatus && player.hasPermission("worldguard.region.toggle-bypass")) {
             player.printInfo(TextComponent.of(
-                    "You are " + (disableBypass ? "not " : "") + "bypassing region protection. " +
-                    "You can toggle this with /rg bypass", TextColor.DARK_PURPLE));
+                    "Ви " + (disableBypass ? "не " : "") + "обходите захист регіону. " +
+                    "Ви можете перемикати це за допомогою /rg bypass", TextColor.DARK_PURPLE));
         }
 
 
