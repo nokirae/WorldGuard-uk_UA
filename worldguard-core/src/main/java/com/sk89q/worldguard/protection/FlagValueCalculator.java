0ef8750dc7e6486ad7cc826d072c89e942f34b39
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 15:20:08 +0300

    Update FlagValueCalculator.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/FlagValueCalculator.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/FlagValueCalculator.java
@@ -389,7 +389,7 @@ public class FlagValueCalculator {
 
         // Check to see whether we have a subject if this is BUILD
         if (flag.requiresSubject() && subject == null) {
-            throw new NullPointerException("The " + flag.getName() + " flag is handled in a special fashion and requires a non-null subject parameter");
+            throw new NullPointerException("Цей " + flag.getName() + " прапор обробляється особливим чином і вимагає не нульового параметра subject");
         }
 
         int minimumPriority = Integer.MIN_VALUE;
