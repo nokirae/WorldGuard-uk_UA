46e048b0027749f3e29bc6a1ba06fa23b5d7c124
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 14:32:27 +0300

    Update WorldHeightMigration.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/migration/WorldHeightMigration.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/migration/WorldHeightMigration.java
@@ -45,7 +45,7 @@ public class WorldHeightMigration extends AbstractMigration {
             maxField = ProtectedRegion.class.getDeclaredField("max");
             maxField.setAccessible(true);
         } catch (NoSuchFieldException e) {
-            throw new ExceptionInInitializerError(new MigrationException("Migrator broke.", e));
+            throw new ExceptionInInitializerError(new MigrationException("Мігратор зламався.", e));
         }
     }
 
@@ -63,14 +63,14 @@ public class WorldHeightMigration extends AbstractMigration {
     protected void migrate(RegionDatabase store) throws MigrationException {
         if (world != null && !store.getName().equals(world.getName())) return;
 
-        log.log(Level.INFO, "Migrating regions in '" + store.getName() + "' to new height limits...");
+        log.log(Level.INFO, "Мігруючі регіони в '" + store.getName() + "' до нових висотних обмежень...");
 
         Set<ProtectedRegion> regions;
 
         try {
             regions = store.loadAll(flagRegistry);
         } catch (StorageException e) {
-            throw new MigrationException("Failed to load region data for the world '" + store.getName() + "'", e);
+            throw new MigrationException("Не вдалося завантажити дані про регіон для світу '" + store.getName() + "'", e);
         }
 
         int min = -64;
@@ -93,7 +93,7 @@ public class WorldHeightMigration extends AbstractMigration {
         try {
             store.saveAll(regions);
         } catch (StorageException e) {
-            throw new MigrationException("Failed to save region data after migration of the world '" + store.getName() + "'", e);
+            throw new MigrationException("Не вдалося зберегти дані регіону після міграції світу '" + store.getName() + "'", e);
         }
     }
 
@@ -103,12 +103,12 @@ public class WorldHeightMigration extends AbstractMigration {
             maxField.set(region, region.getMaximumPoint().withY(max));
             region.setDirty(true);
         } catch (IllegalAccessException e) {
-            throw new MigrationException("Migrator broke.", e);
+            throw new MigrationException("Мігратор зламався.", e);
         }
     }
 
     @Override
     protected void postMigration() {
-        log.log(Level.INFO, "A total of " + changed + " top-to-bottom regions were vertically expanded.");
+        log.log(Level.INFO, "Загалом " + changed + " регіони зверху до низу були вертикально розширені.");
     }
 }
