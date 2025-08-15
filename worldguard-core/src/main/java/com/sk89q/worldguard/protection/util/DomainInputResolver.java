e8eb4b2d3ca78af0e3611a9ecdb1d93acd39ee6a
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 15:15:46 +0300

    Update DomainInputResolver.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/util/DomainInputResolver.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/util/DomainInputResolver.java
@@ -126,14 +126,14 @@ public class DomainInputResolver implements Callable<DefaultDomain> {
                     domain.addPlayer(profile.getUniqueId());
                 }
             } catch (IOException e) {
-                throw new UnresolvedNamesException("The UUID lookup service failed so the names entered could not be turned into UUIDs");
+                throw new UnresolvedNamesException("Пошукова служба UUID не спрацювала, тому введені імена не могли бути перетворені в UUID");
             } catch (InterruptedException e) {
-                throw new UnresolvedNamesException("UUID lookup was interrupted");
+                throw new UnresolvedNamesException("Пошук UUID був перерваний");
             }
         }
 
         if (!namesToQuery.isEmpty()) {
-            throw new UnresolvedNamesException("Unable to resolve the names " + Joiner.on(", ").join(namesToQuery));
+            throw new UnresolvedNamesException("Неможливо визначити імена " + Joiner.on(", ").join(namesToQuery));
         }
 
         return domain;
