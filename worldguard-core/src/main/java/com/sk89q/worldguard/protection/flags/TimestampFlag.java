36e3cc4c70b76a469218fcdd386df1fcccd31e43
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 13:34:45 +0300

    Update TimestampFlag.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/flags/TimestampFlag.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/flags/TimestampFlag.java
@@ -61,10 +61,10 @@ public class TimestampFlag extends Flag<Instant> {
                 } else if (parsed instanceof ZonedDateTime) {
                     return ((ZonedDateTime) parsed).toInstant();
                 } else {
-                    throw new InvalidFlagFormat("Unrecognized input.");
+                    throw new InvalidFlagFormat("Нерозпізнане введення.");
                 }
             } catch (DateTimeParseException ignored) {
-                throw new InvalidFlagFormat("Expected 'now' or ISO 8601 formatted input.");
+                throw new InvalidFlagFormat("Очікувалося 'now' або ISO 8601 форматоване введення.");
             }
         }
     }
