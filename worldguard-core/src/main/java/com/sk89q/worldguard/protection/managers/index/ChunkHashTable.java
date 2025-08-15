0ab395c3d56387940ff358e060447e8d16766930
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 21:02:04 +0300

    Update ChunkHashTable.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/index/ChunkHashTable.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/index/ChunkHashTable.java
@@ -79,7 +79,7 @@ public class ChunkHashTable implements ConcurrentRegionIndex {
      */
     private ListeningExecutorService createExecutor() {
         return MoreExecutors.listeningDecorator(EvenMoreExecutors.newBoundedCachedThreadPool(0, 4, Integer.MAX_VALUE,
-                "WorldGuard Таблиця чанків регіону - " + name));
+                "WorldGuard Region Chunk Table - " + name));
     }
 
     /**
@@ -303,7 +303,7 @@ public class ChunkHashTable implements ConcurrentRegionIndex {
 
         private EnumerateRegions(List<BlockVector2> positions) {
             checkNotNull(positions);
-            checkArgument(!positions.isEmpty(), "Список позицій не може бути порожнім");
+            checkArgument(!positions.isEmpty(), "List of positions can't be empty");
             this.positions = positions;
         }
 
