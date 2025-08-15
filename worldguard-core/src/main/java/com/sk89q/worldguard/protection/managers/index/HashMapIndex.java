af334d61beee7e371011594f51e0d82c56100961
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 14:16:23 +0300

    Update HashMapIndex.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/index/HashMapIndex.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/index/HashMapIndex.java
@@ -175,7 +175,7 @@ public class HashMapIndex extends AbstractRegionIndex implements ConcurrentRegio
                                     try {
                                         current.setParent(replacement);
                                     } catch (CircularInheritanceException e) {
-                                        WorldGuard.logger.log(Level.WARNING, "Failed to replace parent '" + parent.getId() + "' of child '" + current.getId() + "' with replacement '" + replacement.getId() + "'", e);
+                                        WorldGuard.logger.log(Level.WARNING, "Не вдалося замінити батьківський елемент '" + parent.getId() + "' для наслідкового '" + current.getId() + "' з заміною '" + replacement.getId() + "'", e);
                                         current.clearParent();
                                     }
                             }
