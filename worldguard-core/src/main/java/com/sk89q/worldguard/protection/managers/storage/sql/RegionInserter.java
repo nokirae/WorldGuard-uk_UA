3124e7fa7e8b150bc04b07a91ba0a67379c8e92d
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 14:45:10 +0300

    Update RegionInserter.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/storage/sql/RegionInserter.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/storage/sql/RegionInserter.java
@@ -69,7 +69,7 @@ class RegionInserter {
             // Nothing special to do about them
 
         } else {
-            throw new IllegalArgumentException("Unknown type of region: " + region.getClass().getName());
+            throw new IllegalArgumentException("Невідомий тип регіону: " + region.getClass().getName());
         }
     }
 
