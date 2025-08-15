7a170d204b62302b1e1e8c3d408ef7d9678d3e03
nokirae <44533308+nokirae@users.noreply.github.com>
2025-08-15 14:33:18 +0300

    Update DirectoryYamlDriver.java

--- worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/storage/file/DirectoryYamlDriver.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/protection/managers/storage/file/DirectoryYamlDriver.java
@@ -66,7 +66,7 @@ public class DirectoryYamlDriver implements RegionDriver {
             f.getCanonicalPath();
             return f;
         } catch (IOException e) {
-            throw new IllegalArgumentException("Invalid file path for the world's regions file");
+            throw new IllegalArgumentException("Недійсний шлях до файлу регіонів світу");
         }
     }
 
